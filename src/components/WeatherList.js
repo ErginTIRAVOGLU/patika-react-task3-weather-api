c086dd9a793709007451826c35aab9616fdeed4e
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-11 15:37:26 +0300

    init

---
+++ src/components/WeatherList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function WeatherList() {
+    //api.openweathermap.org/data/2.5/forecast/daily?q={city name},{country code}&cnt={cnt}&appid={API key}
+  return (
+    <div>WeatherList</div>
+  )
+}
+
+export default WeatherList
